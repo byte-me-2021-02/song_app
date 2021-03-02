21e9c4f3438166985b781ee9ed1d2b23b43e17fe
brian <riceb53@gmail.com>
2021-03-01 20:08:13 -0500

    songs create

--- app/controllers/api/songs_controller.rb
+++ app/controllers/api/songs_controller.rb
@@ -3,4 +3,15 @@ class Api::SongsController < ApplicationController
     @songs = Song.all
     render 'index.json.jb'
   end
+
+  def create
+    @song = Song.new(
+      title: params[:title],
+      album: params[:album],
+      artist: params[:artist],
+      year: params[:year] 
+    )
+    @song.save
+    render 'show.json.jb'
+  end
 end
