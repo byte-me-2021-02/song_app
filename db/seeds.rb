0e09794fc772dec6bcb2434933e6fcb8251ce763
brian <riceb53@gmail.com>
2021-03-01 19:50:35 -0500

    generate Song model and make seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+song = Song.new(title: "eye of the tiger", album: "1980s hits", artist: "survivor", year: 1981)
+song.save
+
+song = Song.new(title: "baby (feat. ludacris)", album: "2010s hits", artist: "justin bieber", year: 2010)
+song.save
+
+song = Song.new(title: "barbara ann", album: "1970s hits", artist: "the beach boys", year: 1973)
+song.save
