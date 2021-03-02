81fc9828cdf2171df344d784f851fff4cd8d7a4a
brian <riceb53@gmail.com>
2021-03-01 20:17:54 -0500

    allow user to update single attributes

--- app/controllers/api/songs_controller.rb
+++ app/controllers/api/songs_controller.rb
@@ -22,10 +22,10 @@ class Api::SongsController < ApplicationController
 
   def update
     @song = Song.find_by(id: params[:id])
-    @song.title = params[:title]
-    @song.album = params[:album]
-    @song.artist = params[:artist]
-    @song.year = params[:year]
+    @song.title = params[:title] || @song.title
+    @song.album = params[:album] || @song.album
+    @song.artist = params[:artist] || @song.artist
+    @song.year = params[:year] || @song.year
     @song.save
     render 'show.json.jb'
   end
